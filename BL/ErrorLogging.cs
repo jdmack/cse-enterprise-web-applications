2231cd4edcae0f99d6da271d97b73d21c20add89
Jun Abbott <jun.abbott@gmail.com>
2012-08-29 22:51:25 -0700

    ErrorLogging completed

--- BL/ErrorLogging.cs
+++ BL/ErrorLogging.cs
@@ -14,89 +14,97 @@ using System.Configuration; // must add this... make sure to add "System.Configu
 ///
 namespace BL
 {
-	// this is the interface definition
-	public interface IErrorLogging
-	{
-		void LogError(List<string> errorList);
-	}
+    // this is the interface definition
+    public interface IErrorLogging
+    {
+        void LogError(List<string> errorList);
+    }
 
-	public class LogToFile : IErrorLogging
-	{
-		public void LogError(List<string> errorList)
-		{
-			// 136 Students TODO: Write to local file system somewhere
+    public class LogToFile : IErrorLogging
+    {
+        public void LogError(List<string> errorList)
+        {
+            // 136 Students TODO: Write to local file system somewhere
             StreamWriter writer = new StreamWriter("c:\\errorlog.txt");
             foreach (string error in errorList)
             {
                 writer.WriteLine(error);
             }
             writer.Close();
-		}
-	}
+        }
+    }
 
-	public class LogToDB : IErrorLogging
-	{
-		public void LogError(List<string> errorList)
-		{
-			// 136 Students TODO: write to database table.
-            SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["dsn"]);
-            try
+    public class LogToDB : IErrorLogging
+    {
+        public void LogError(List<string> errorList)
+        {
+            // 136 Students TODO: write to database table.
+            
+
+            foreach (string error in errorList)
             {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["dsn"]);
                 string strSQL = "spInsertLog";
+                
+                try
+                {
+                    SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
+                    mySA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    mySA.SelectCommand.Parameters.Add(new SqlParameter("@log", SqlDbType.VarChar, 100));
+                    mySA.SelectCommand.Parameters["@log"].Value = error;
 
-                SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
-                mySA.SelectCommand.CommandType = CommandType.StoredProcedure;
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@matchup", SqlDbType.Char, 3));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@time", SqlDbType.DateTime));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@length", SqlDbType.VarChar, 50));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@player1", SqlDbType.Int));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@player1_race", SqlDbType.Int));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@player2", SqlDbType.Int));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@player2_race", SqlDbType.Int));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@winner", SqlDbType.Int));
-                mySA.SelectCommand.Parameters.Add(new SqlParameter("@map", SqlDbType.Int));
+                    DataSet myDS = new DataSet();
+                    mySA.Fill(myDS);
+                }
+                catch (Exception e)
+                {
+          
+                }
+                finally
+                {
+                    conn.Dispose();
+                    conn = null;
+                }
+            }
 
-                mySA.SelectCommand.Parameters["@matchup"].Value = game.matchup;
-                mySA.SelectCommand.Parameters["@time"].Value = game.time;
-                mySA.SelectCommand.Parameters["@length"].Value = game.length;
 
-		}
-	}
+        }
+    }
 
-	public class ErrorLogFactory
-	{
+    public class ErrorLogFactory
+    {
 
-		static string logDestination = ConfigurationManager.AppSettings["logDestination"];
-		public IErrorLogging logInstance = null;
+        static string logDestination = ConfigurationManager.AppSettings["logDestination"];
+        public IErrorLogging logInstance = null;
 
-		public IErrorLogging GetErrorLogInstance()
-		{
-			switch (logDestination)
-			{
-				case "file":
-					logInstance = new LogToFile();
-					break;
-				case "db":
-					logInstance = new LogToDB();
-					break;
-				default:
-					break;
-			}
-			return logInstance;
-		}
-	}
+        public IErrorLogging GetErrorLogInstance()
+        {
+            switch (logDestination)
+            {
+                case "file":
+                    logInstance = new LogToFile();
+                    break;
+                case "db":
+                    logInstance = new LogToDB();
+                    break;
+                default:
+                    break;
+            }
+            return logInstance;
+        }
+    }
 
-	public class AsynchLog
-	{
-		delegate void MethodDelegate(List<string> strError);
+    public class AsynchLog
+    {
+        delegate void MethodDelegate(List<string> strError);
 
-		public static void LogNow(List<string> strError)
-		{
-			IErrorLogging log = new ErrorLogFactory().GetErrorLogInstance();
+        public static void LogNow(List<string> strError)
+        {
+            IErrorLogging log = new ErrorLogFactory().GetErrorLogInstance();
 
-			MethodDelegate callGenerateFileAsync = new MethodDelegate(log.LogError);
-			IAsyncResult ar = callGenerateFileAsync.BeginInvoke(strError, null, null);
-		}
-	}
+            MethodDelegate callGenerateFileAsync = new MethodDelegate(log.LogError);
+            IAsyncResult ar = callGenerateFileAsync.BeginInvoke(strError, null, null);
+        }
+    }
 
 }
